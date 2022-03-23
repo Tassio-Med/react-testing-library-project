0d5f29f31a09b784af561fc15897b4b18c7c9cd3
Tassio Med <tassiohfmed@gmail.com>
2022-03-22 21:52:38 -0300

    requisito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './support/renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente "FavoritePokemons.js"', () => {
+  it('Testa se a página contém um heading h2 com o texto'
+  + '"Page requested not found😭"', () => {
+    renderWithRouter(<NotFound />);
+    const heading = screen
+      .getByRole('heading', { level: 2, name: /Page requested not found/i });
+    expect(heading).toBeInTheDocument();
+  });
+  it('Teste se página mostra uma imagem em formato GIF', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen.getByRole('img', { name: /Pikachu/i });
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
